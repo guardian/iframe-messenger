c5702aa5269d91845ba2dbb74a5b6b4240434eea
andymason <andrew.mason@guardian.co.uk>
2014-02-05 18:16:23 +0000

    Added unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     qunit: {
-      all: ['test/**/*.html']
+      all: ['test/*.html']
     }
   });
 
--- test/qunit_test.js
+++ test/qunit_test.js
@@ -1,15 +1,56 @@
-test("iframe", function() {
-	ok(window.iframeMessenger, "Loaded ok!");
+asyncTest("iframeMessanger .resize()", function() {
+	window.
+	window.addEventListener('message', function(event) {
+		var data = JSON.parse(event.data);
+		expect(2);
+		equal(data['type'], 'set-height', 'should send set-height type.');
+		equal(data['value'], '900', 'should send value of 900.');
+
+		// Clear event listener
+		this.removeEventListener('message', arguments.callee,false);
+
+		start();
+	});
+
+	var iframe = document.querySelector("#qunit-fixture iframe");
+	iframe.src = 'iframeContent/resizeDemo.html';
 });
 
-test('basic test', function() {
-  expect(1);
-  ok(true, 'this had better work.');
+
+asyncTest("iframeMessanger enableAutoResize with absoluteHeight set", function() {
+	window.addEventListener('message', function(event) {
+		var data = JSON.parse(event.data);
+		expect(2);
+		equal(data['type'], 'set-height', 'should send set-height type.');
+		equal(data['value'], '500', 'should have 500px height.');
+
+		// Clear event listener
+		this.removeEventListener('message', arguments.callee,false);
+
+		start();
+	});
+
+	var iframe = document.querySelector("#qunit-fixture iframe");
+	iframe.src = 'iframeContent/absolutePositionDemo.html';
 });
 
 
-test('can access the DOM', function() {
-  expect(1);
-  var fixture = document.getElementById('qunit-fixture');
-  equal(fixture.innerText, 'this had better work.', 'should be able to access the DOM.');
+asyncTest("iframeMessanger .navigate()", function() {
+	window.
+	window.addEventListener('message', function(event) {
+		var data = JSON.parse(event.data);
+		expect(2);
+		equal(data['type'], 'navigate', 'should send the navigate type.');
+		equal(data['value'], 'http://www.example.com/', 'should send correct string value.');
+
+		// Clear event listener
+		this.removeEventListener('message', arguments.callee,false);
+
+		start();
+	});
+
+	var iframe = document.querySelector("#qunit-fixture iframe");
+	iframe.src = 'iframeContent/navigateDemo.html';
 });
+
+
